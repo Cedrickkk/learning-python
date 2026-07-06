cd131bd38ae5f331d60f5a04f7e96e3693858fd4
hulaanmo <inhog.cedrick@gmail.com>
2026-07-07 07:45:07 +0800

    list name binding

--- week-1/02-objects-names-and-dynamic-typing/main.py
+++ week-1/02-objects-names-and-dynamic-typing/main.py
@@ -4,5 +4,18 @@ def main():
     print(id(x))
     print(id(y))
 
+    a = [1, 2, 3]
+    b = a
+    b.append(4)
+    print(a)
+    print(b)
+
+    my_list = ["Python"]
+    add_item(my_list)
+    print(my_list)
+
+def add_item(items):
+    items.append("AI")
+
 if __name__ == "__main__":
     main()
