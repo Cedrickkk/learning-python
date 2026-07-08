3c3e992b1018ef874f3577cd18bca66d8a14a683
hulaanmo <inhog.cedrick@gmail.com>
2026-07-08 08:23:25 +0800

    test function return type

--- week-1/02-objects-names-and-dynamic-typing/functions.py
+++ week-1/02-objects-names-and-dynamic-typing/functions.py
@@ -1,8 +1,13 @@
 def main():
-    print(type(greet)) # <class 'function'>
- 
+    result = greet_1("Cedrick")
+    print(result) # None
+    print(type(result)) # <class 'NoneType'>
+    
 def greet():
     print("Hello!")
+
+def greet_1(name):
+    print(f"Hello {name}")
 
 if __name__ == '__main__':
     main()
