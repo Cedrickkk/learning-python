4f1d14f99e8ad19701f1232ec4dce767a2777443
hulaanmo <inhog.cedrick@gmail.com>
2026-07-09 20:50:55 +0800

    add list manipulation exercise

--- week-1/02-objects-names-and-dynamic-typing/loops.py
+++ week-1/02-objects-names-and-dynamic-typing/loops.py
@@ -1,8 +1,26 @@
 def main():
-    numbers = [1, 2, 3]
+    exercise_1()
+
+    test = ["hello", "john", "cedrick"]
+    
+    for i in range(5):
+        print(i)
+    
+    print(range(5))
+    print(type(range(5)))
+
+    numbers = range(5)
+
+    print(numbers)
+    print(list(numbers))
+
+def exercise_1():
+    numbers = [[1], [2], [3]]
 
     for number in numbers:
-        print(number)
+        number.append(100)
+
+    print(numbers)
 
 if __name__ == '__main__':
     main()
