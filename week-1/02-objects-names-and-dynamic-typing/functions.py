845b1dbcca196e2319a98990e18c213d4f54b758
hulaanmo <inhog.cedrick@gmail.com>
2026-07-09 08:22:57 +0800

    test function arguments identity

--- week-1/02-objects-names-and-dynamic-typing/functions.py
+++ week-1/02-objects-names-and-dynamic-typing/functions.py
@@ -1,13 +1,23 @@
 def main():
-    result = greet_1("Cedrick")
-    print(result) # None
-    print(type(result)) # <class 'NoneType'>
+    print(add_item('A'))
+    print(add_item('B'))
+    print(add_item('C'))
     
+
 def greet():
     print("Hello!")
 
 def greet_1(name):
     print(f"Hello {name}")
+
+def add_item(item, items = None):
+    if items is None:
+        items = []
+
+    print(id(items))
+    
+    items.append(item)
+    return items
 
 if __name__ == '__main__':
     main()
